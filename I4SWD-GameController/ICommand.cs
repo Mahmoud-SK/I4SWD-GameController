d18ecf4f493b9518e18951d9fb3119086fa7f58a
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-04-06 13:09:14 +0200

    Add project files.

---
+++ I4SWD-GameController/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I4SWD_GameController
+{
+	interface ICommand
+	{
+		void Execute();
+	}
+}
---
+++ I4SWD-GameController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I4SWD_GameController
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
