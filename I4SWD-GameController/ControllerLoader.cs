1ceedacebf19216bbb3026d4ed9d4e2d27b84ca6
Lasseerenged <201806450@post.au.dk>
2021-04-08 20:50:13 +0200

    Tilføjet kommentarer

--- I4SWD-GameController/ControllerLoader.cs
+++ I4SWD-GameController/ControllerLoader.cs
@@ -8,22 +8,25 @@ namespace I4SWD_GameController
 	{
 		static void Main(string[] args)
 		{
+			//opretelse af Receivers
 			GameCharacter GC = new GameCharacter();
 			Menu menu = new Menu();
 			Building building = new Building();
 
+			//oprettelse af commands
 			AttackCommand atk = new AttackCommand(GC);
 			JumpCommand jmp = new JumpCommand(GC);
 			OpenMenuCommand openMenu = new OpenMenuCommand(menu);
 			UpgradeCommand upgrade = new UpgradeCommand(building);
 
-
 			Controller control = new Controller();
+			//her bindes de forskellige commands til hver deres knap.
 			control.setCommand(jmp, 0);
 			control.setCommand(atk, 1);
 			control.setCommand(openMenu, 2);
 			control.setCommand(upgrade, 3);
 
+			//Test hvor der trykkes på hver knap en gang.
 			control.buttonPushed(0);
 			control.buttonPushed(1);
 			control.buttonPushed(2);
